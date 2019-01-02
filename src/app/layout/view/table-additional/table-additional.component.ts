d67241b77aa4a48a47cae9f2a30ed6c33e958044
oleht <oleh.tsyupa@gmail.com>
2019-01-02 20:52:50 +0100

    Newest + recommended articles. First step

--- src/app/layout/view/table-additional/table-additional.component.ts
+++ src/app/layout/view/table-additional/table-additional.component.ts
@@ -2,10 +2,10 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {AdditionalDataService} from "../../../service/backend/article/additional.data.service";
 import {ArticleType} from "../../../domain/emun/article.type";
 import {ImageService} from "../../../service/backend/article/image.service";
-import {Observable, Subject} from "rxjs/index";
-import {share, subscribeOn, takeUntil, tap} from "rxjs/internal/operators";
-import {ArticleAdditional} from "../../../domain/dto/article/article.additional";
+import {Subject} from "rxjs/index";
+import {takeUntil} from "rxjs/internal/operators";
 import {ArticleShort} from "../../../domain/dto/article/article.short";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-table-additional',
@@ -25,24 +25,38 @@ export class TableAdditionalComponent implements OnInit, OnDestroy {
   private componentDestroyed: Subject<any> = new Subject();
 
   constructor(private imageService: ImageService,
+              private route: ActivatedRoute,
               private additionalDataService: AdditionalDataService) {
+
   }
 
   ngOnInit() {
-    console.log(this.articleType);
-    this.getAdditionalArticleDataMethod()
+    console.log(this.route);
+    this.route.params
       .pipe(takeUntil(this.componentDestroyed))
-      .subscribe(next => {
-          console.log(next);
-          this.firstPartRecommended = next.recommended.slice(0, 2);
-          this.secondPartRecommended = next.recommended.slice(2, next.recommended.length);
-          this.newest = next.newest;
-        });
+      .subscribe(params => {
+        if (params['topicId']) {
+          this.topicId = params['topicId'];
+        } else if (params['tagId']) {
+          this.tagId = params['tagId'];
+        }
+        this.loadData();
+      });
   }
 
   ngOnDestroy() {
     this.componentDestroyed.next();
     this.componentDestroyed.unsubscribe();
+  }
+
+  loadData() {
+    this.getAdditionalArticleDataMethod()
+      .pipe(takeUntil(this.componentDestroyed))
+      .subscribe(next => {
+        this.firstPartRecommended = next.recommended.slice(0, 2);
+        this.secondPartRecommended = next.recommended.slice(2, next.recommended.length);
+        this.newest = next.newest;
+      });
   }
 
   getAdditionalArticleDataMethod() {
